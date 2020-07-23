ed9024d3cde2a9fecd21b07edaf7830c65808e6b
rogurotus <61418195+rogurotus@users.noreply.github.com>
2020-07-23 16:05:31 +0700

    back2

---
+++ Proto/build-work_proto-Desktop_Qt_5_12_6_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,126 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.6
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QVBoxLayout *verticalLayout;
+    QLabel *login_label;
+    QLineEdit *login_edit;
+    QLabel *password_label;
+    QLineEdit *password_edit;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(250, 250);
+        MainWindow->setMinimumSize(QSize(250, 250));
+        MainWindow->setMaximumSize(QSize(250, 250));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        verticalLayout = new QVBoxLayout(centralwidget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        login_label = new QLabel(centralwidget);
+        login_label->setObjectName(QString::fromUtf8("login_label"));
+
+        verticalLayout->addWidget(login_label);
+
+        login_edit = new QLineEdit(centralwidget);
+        login_edit->setObjectName(QString::fromUtf8("login_edit"));
+
+        verticalLayout->addWidget(login_edit);
+
+        password_label = new QLabel(centralwidget);
+        password_label->setObjectName(QString::fromUtf8("password_label"));
+        password_label->setMinimumSize(QSize(0, 0));
+        password_label->setMaximumSize(QSize(10000, 10000));
+
+        verticalLayout->addWidget(password_label);
+
+        password_edit = new QLineEdit(centralwidget);
+        password_edit->setObjectName(QString::fromUtf8("password_edit"));
+
+        verticalLayout->addWidget(password_edit);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+
+        horizontalLayout->addWidget(pushButton);
+
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+
+        horizontalLayout->addWidget(pushButton_2);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 250, 20));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "\320\220\321\203\321\202\320\265\320\275\321\202\320\270\321\204\320\270\320\272\320\260\321\206\320\270\321\217", nullptr));
+        login_label->setText(QApplication::translate("MainWindow", "\320\222\320\262\320\265\320\264\320\270\321\202\320\265 \320\273\320\276\320\263\320\270\320\275", nullptr));
+        password_label->setText(QApplication::translate("MainWindow", "\320\222\320\262\320\265\320\264\320\270\321\202\320\265 \320\277\320\260\321\200\320\276\320\273\321\214", nullptr));
+#ifndef QT_NO_TOOLTIP
+        pushButton->setToolTip(QApplication::translate("MainWindow", "\320\222\320\276\320\271\321\202\320\270 \320\272\320\260\320\272 \320\272\320\276\320\274\320\265\320\275\320\273\320\260\320\275\321\202. \320\242\321\200\320\265\320\261\321\203\320\265\321\202\321\201\321\217 \320\260\321\203\321\202\320\265\320\275\321\202\320\270\321\204\320\270\320\272\320\260\321\206\320\270\321\217.", nullptr));
+#endif // QT_NO_TOOLTIP
+        pushButton->setText(QApplication::translate("MainWindow", "\320\222\320\276\320\271\321\202\320\270", nullptr));
+#ifndef QT_NO_TOOLTIP
+        pushButton_2->setToolTip(QApplication::translate("MainWindow", "\320\230\320\275\321\204\320\276\321\200\320\274\320\260\321\206\320\270\321\217 \320\276\320\261 \320\276\320\261\321\211\320\265\320\266\320\270\321\202\320\270\321\217\321\205 \320\264\320\273\321\217 \320\263\320\276\321\201\321\202\320\265\320\271. \320\235\320\265 \321\202\321\200\320\265\320\261\321\203\320\265\321\202\321\201\321\217 \320\260\321\203\321\202\320\265\320\275\321\202\320\270\321\204\320\270\320\272\320\260\321\206\320\270\321\217.", nullptr));
+#endif // QT_NO_TOOLTIP
+        pushButton_2->setText(QApplication::translate("MainWindow", "\320\222\320\276\320\271\321\202\320\270 \320\272\320\260\320\272 \320\263\320\276\321\201\321\202\321\214", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
---
+++ Proto/build-work_proto-Desktop_Qt_5_12_6_MinGW_64_bit-Debug/ui_view.h
@@ -0,0 +1,72 @@
+/********************************************************************************
+** Form generated from reading UI file 'view.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.6
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_VIEW_H
+#define UI_VIEW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QTableView>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_View
+{
+public:
+    QVBoxLayout *verticalLayout;
+    QTableView *tableView;
+    QSpacerItem *verticalSpacer;
+    QLabel *state_label;
+
+    void setupUi(QWidget *View)
+    {
+        if (View->objectName().isEmpty())
+            View->setObjectName(QString::fromUtf8("View"));
+        View->resize(720, 455);
+        verticalLayout = new QVBoxLayout(View);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        tableView = new QTableView(View);
+        tableView->setObjectName(QString::fromUtf8("tableView"));
+
+        verticalLayout->addWidget(tableView);
+
+        verticalSpacer = new QSpacerItem(20, 20, QSizePolicy::Minimum, QSizePolicy::Fixed);
+
+        verticalLayout->addItem(verticalSpacer);
+
+        state_label = new QLabel(View);
+        state_label->setObjectName(QString::fromUtf8("state_label"));
+
+        verticalLayout->addWidget(state_label);
+
+
+        retranslateUi(View);
+
+        QMetaObject::connectSlotsByName(View);
+    } // setupUi
+
+    void retranslateUi(QWidget *View)
+    {
+        View->setWindowTitle(QApplication::translate("View", "Form", nullptr));
+        state_label->setText(QApplication::translate("View", "\320\232\320\276\320\274\320\265\320\275\320\264\320\260\320\275\321\202:", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class View: public Ui_View {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_VIEW_H
